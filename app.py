7170879d0bb46e1feb1cce7360a572129f04999d
KemmieKemy <kemiokpongkpong@gmail.com>
2020-03-26 08:50:33 +0100

    changed HTML file

--- app.py
+++ app.py
@@ -14,4 +14,6 @@ def predict():
     A = [float(x) for x in request.form.values()]
     model_probability = model.predict_proba([A])
     prediction = "Probability of  this user having Diabetes is %0.2f"%model_probability[0][1]
-    return render_template('index.html',result = prediction)
+    return render_template('index.html', result = prediction)
+if __name__ == "__main__":
+    app.run(debug=True)
