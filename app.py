93e1930d7b33800ccc36b00876e51588e5c703e1
KemmieKemy <kemiokpongkpong@gmail.com>
2020-03-26 08:03:03 +0100

    committed

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+# prediction function   
+@app.route('/predict', methods = ['POST']) 
+def predict(): 
+    A = [float(x) for x in request.form.values()]
+    model_probability = model.predict_proba([A])
+    prediction = "Probability of  this user having Diabetes is %0.2f"%model_probability[0][1]
+    return render_template('index.html',result = prediction)
+    
+if __name__ == '__main__':
+        app.run(debug=True)
